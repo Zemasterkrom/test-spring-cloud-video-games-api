6facf3928d9b9f8e8769330564303c2e950d9dd5
Zemasterkrom <raphael.kimm@outlook.fr>
2021-12-07 14:06:37 +0100

    Ajout et liaison du serveur de découverte

---
+++ vgldiscovery/src/main/java/com/rk/vgldiscovery/VideoGameLibraryDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.rk.vgldiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class VideoGameLibraryDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoGameLibraryDiscoveryApplication.class, args);
+    }
+
+}
