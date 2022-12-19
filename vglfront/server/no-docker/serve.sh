e12d7abf84e2934966167be205d936ca68f67467
Zemasterkrom <raphael.kimm@outlook.fr>
2022-12-19 22:52:24 +0100

    wait for API to be up (darwin), load balancing activated by default

--- vglfront/server/no-docker/serve.sh
+++ vglfront/server/no-docker/serve.sh
@@ -2,15 +2,15 @@
 set -e
 
 touch .env
-source .env
+. ./.env
 
 javac server/FileEnvironmentConfigurator.java
 java -cp .. vglfront.server.FileEnvironmentConfigurator src/assets/environment.js @JSKEY@ @JSVALUE@ "window['environment']['@JSKEY@'] = '@JSVALUE@';" url ${API_URL} http://localhost:8080
 
 echo "Waiting for ${API_URL:-http://localhost:8080} to be up ..."
-API_URL = $(echo "${API_URL}" | sed -E "s/(http[s]?://)(.*)/\1-get\2/g")
-if ! [ $(echo "${API_URL}" | grep -E "http[s]?://.*") ]; then
-  API_URL = "http-get://${API_URL}"
+API_URL=$(echo "${API_URL:-http://localhost:8080}" | sed -E "s/(http[s]?):\/\/(.*)/\1-get:\/\/\2/g")
+if ! [ $(echo "${API_URL}" | grep -E "http[s]?(-get)?://.*") ]; then
+  API_URL="http-get://${API_URL}"
 fi
 
-wait-on ${API_URL:-http-get://localhost:8080}/video-games/all -t 120000 -i 10000 && ng serve --port ${FRONT_SERVER_PORT:-4200}
+wait-on ${API_URL}/video-games/all -t 120000 -i 10000 && ng serve --port ${FRONT_SERVER_PORT:-4200}
