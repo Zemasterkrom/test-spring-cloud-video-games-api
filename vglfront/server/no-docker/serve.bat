2aa6a7537a0f0dc57fe812175497fd2746977a83
Zemasterkrom <raphael.kimm@outlook.fr>
2022-12-19 22:19:56 +0100

    wait for API to be up inside the no-docker run mode

--- vglfront/server/no-docker/serve.bat
+++ vglfront/server/no-docker/serve.bat
@@ -1,20 +1,27 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET FRONT_SERVER_PORT=4200
-
-type NUL >> .env
-if %errorlevel% neq 0 exit /b %errorlevel%
+SET API_URL=http://localhost:8080
+TYPE NUL >> .env
+IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
 
 FOR /F "tokens=* eol=#" %%i IN ('type .env') DO (
     SET %%i
-    if !errorlevel! neq 0 exit /b !errorlevel!
+    IF !errorlevel! NEQ 0 EXIT /b !errorlevel!
 )
 
 javac server/FileEnvironmentConfigurator.java
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
 
 java -cp .. vglfront.server.FileEnvironmentConfigurator src/assets/environment.js @JSKEY@ @JSVALUE@ "window['environment']['@JSKEY@'] = '@JSVALUE@';" url %API_URL% http://localhost:8080
-if %errorlevel% neq 0 exit /b %errorlevel%
+IF %errorlevel% NEQ 0 EXIT /b %errorlevel%
 
-ng serve --port %FRONT_SERVER_PORT%
+echo Waiting for %API_URL% to be up ...
+
+SET API_URL=%API_URL:http://=http-get://%
+SET API_URL=%API_URL:https://=https-get://%
+Echo.%API_URL%| findstr /R /C:"^http-get://[a-Z0-9]*$" /C:"^https-get://[a-Z0-9]*$">NUL & IF %errorlevel% NEQ 0 (
+    SET API_URL=http-get://%API_URL%
+)
+wait-on %API_URL%/video-games/all -t 120000 -i 10000 && ng serve --port %FRONT_SERVER_PORT%
 ENDLOCAL
--- vglfront/server/no-docker/serve.sh
+++ vglfront/server/no-docker/serve.sh
@@ -3,6 +3,14 @@ set -e
 
 touch .env
 source .env
+
 javac server/FileEnvironmentConfigurator.java
 java -cp .. vglfront.server.FileEnvironmentConfigurator src/assets/environment.js @JSKEY@ @JSVALUE@ "window['environment']['@JSKEY@'] = '@JSVALUE@';" url ${API_URL} http://localhost:8080
-ng serve --port ${FRONT_SERVER_PORT:-4200}
+
+echo "Waiting for ${API_URL:-http://localhost:8080} to be up ..."
+API_URL = $(echo "${API_URL}" | sed -E "s/(http[s]?://)(.*)/\1-get\2/g")
+if ! [ $(echo "${API_URL}" | grep -E "http[s]?://.*") ]; then
+  API_URL = "http-get://${API_URL}"
+fi
+
+wait-on ${API_URL:-http-get://localhost:8080}/video-games/all -t 120000 -i 10000 && ng serve --port ${FRONT_SERVER_PORT:-4200}
